c6b1f7b4400945d6af639cdb436f19e5c795b522
Samy Zogeyb <contact@samyzogeyb.com>
2021-08-18 16:29:17 +0400

    added global theme support for chakra

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { Provider as StoreProvider } from "react-redux";
 import AuthProvider from "../context/AuthProvider";
 import store from "../store/rootreducer";
+import global from "../utils/chakra";
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<StoreProvider store={store}>
 			<AuthProvider>
-				<ChakraProvider>
+				<ChakraProvider theme={global}>
 					<Component {...pageProps} />
 				</ChakraProvider>
 			</AuthProvider>
---
+++ src/utils/chakra.js
@@ -0,0 +1,17 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export default global = extendTheme({
+	styles: {
+		global: {
+			body: {
+				display: "flex",
+				justifyContent: "center",
+			},
+			"#__next": {
+				w: "600px",
+				minW: "250px",
+				minH: "100vh",
+			},
+		},
+	},
+});
